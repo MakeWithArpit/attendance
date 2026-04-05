2ba0deac621e2a2e6ec4027b0c291a913805e33a
Arpit <arpit.gangwar061@gmail.com>
2026-04-05 12:28:09 +0530

    update url on api.js

--- Frontend/api.js
+++ Frontend/api.js
@@ -2,7 +2,7 @@
  * api.js — AttendX Shared Utilities
  * Invertis University, Bareilly — Attendance Management System
  */
-const API_BASE = 'http://127.0.0.1:8000/api/';
+const API_BASE = 'https://gymnospermal-patronymically-val.ngrok-free.dev/api/';
 
 const Auth = {
   save(d) {
